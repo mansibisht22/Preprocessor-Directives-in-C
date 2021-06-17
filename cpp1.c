5ea50296638e186ce00650136577d81e1f1efbd5
Mansi Bisht <84663893+mansibisht22@users.noreply.github.com>
2021-06-17 11:34:55 +0530

    Add files via upload

---
+++ cpp1.c
@@ -0,0 +1,14 @@
+// macro definition for lower case
+#define LOWER(alpha) (alpha>=97 && alpha<=122)
+
+// macro definition for upper case
+#define UPPER(alpha) (alpha>=65 && alpha<=90)
+
+//Whether alphabet or not
+#define LOWER(alpha) (alpha>=97 && alpha<=122)
+#define UPPER(alpha) (alpha>=65 && alpha<=90)
+#define YES (LOWER || UPPER)
+
+//Obtain the bigger number
+#define MAX(a,b) (a>b?a:b)
+
